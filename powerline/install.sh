a993918381435115d89d7c73568d55fb4400b23d
Jose Hidalgo <joseche@gmail.com>
2016-07-07 21:31:10 -0600

    fixes

--- powerline/install.sh
+++ powerline/install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-[ ! -d ~/tmp ] && mkdir ~/tmp
-cd ~/tmp
-[ -d fonts ] && rm -rf fonts
-git clone https://github.com/powerline/fonts
-cd fonts
-source ./install.sh
+if [ -f "~/Library/Fonts/Hack-Boldttf" ]; then
+    [ ! -d ~/tmp ] && mkdir ~/tmp
+    cd ~/tmp
+    [ -d fonts ] && rm -rf fonts
+    git clone https://github.com/powerline/fonts
+    cd fonts
+    source ./install.sh
+fi
 
 pip install powerline-status --user
 
