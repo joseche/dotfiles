494009b9b7499cb665f9be07a14ac71ac3a2b590
Jose Hidalgo <joseche@gmail.com>
2016-07-07 19:55:02 -0600

    fixed many issues

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,23 +2,15 @@
 #
 # Homebrew
 #
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
+if test "$(uname -s)" = "Darwin"
 then
-  echo "  Installing Homebrew"
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-      exit 0
-  fi
+    if test ! $(which brew)
+    then
+      echo "  Installing Homebrew"
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    cd "$(dirname $0)"
+    brew tap homebrew/bundle
+    brew bundle
 fi
-
-cd "$(dirname $0)"
-brew tap homebrew/bundle
-brew bundle
--- powerline/install.sh
+++ powerline/install.sh
@@ -7,5 +7,5 @@ git clone https://github.com/powerline/fonts
 cd fonts
 source ./install.sh
 
-pip install powerline
+pip install powerline-status --user
 
