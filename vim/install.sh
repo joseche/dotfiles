e1731879357b6e9c74da1292715dea3ee4b79c53
Jose Hidalgo <joseche@gmail.com>
2016-07-11 21:12:54 -0600

    unified configuration neovim/vim

--- vim/install.sh
+++ vim/install.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 # Configure vim and neovim
+set -e
 
-# VIM 
+# VIM
 echo Configuring vim
-[ ! -r ~/.vim/bundle/Vundle.vim ] && \
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-\vim +PluginInstall +qall
+# [ ! -r ~/.vim/bundle/Vundle.vim ] && \
+#     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[ -d ~/.vim/autoload ] && rm -rf ~/.vim/autoload
+curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+\vim +PlugInstall +qall
 
 # NEOVIM
 echo Configuring nvim
-[ ! -d ~/.config/nvim ] && mkdir -p ~/.config/nvim
-[ ! -r ~/.config/nvim/init.vim ] && ln -s "$(dirname $0)/nvim.init" ~/.config/nvim/init.vim
-[ ! -r ~/.config/nvim/autoload/plug.vim ] && curl -s -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+[ -d ~/.config/nvim ] && rm -rf ~/.config/nvim
+curl -s -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s "$(dirname $0)/vimrc.symlink" ~/.config/nvim/init.vim
 nvim +PlugInstall +qall
+
+set +e
