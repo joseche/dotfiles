aa56fb71d970a0418b2cbcf8d9e0b7facec0aeff
Jose Hidalgo <c0d5x@protonmail.com>
2016-07-20 08:35:37 -0600

    first nvim for async downloads

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# VIM
-echo -e "\tConfiguring vim"
-[ -d ~/.vim/autoload ] && rm -rf ~/.vim/autoload
-curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-\vim +PlugInstall +qall
-
-# NEOVIM
-echo -e "\tConfiguring nvim"
+# Neovim
+printf  "\tConfiguring nvim\n"
 [ -d ~/.config/nvim ] && rm -rf ~/.config/nvim
 curl -s -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 nvim +PlugInstall +qall
+
+# Vim
+printf  "\tConfiguring vim\n"
+[ -d ~/.vim ] && rm -rf ~/.vim
+curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+\vim +PlugInstall +qall
