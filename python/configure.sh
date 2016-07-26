36fc0412a5f2e8cec43bafe3299a933b2582290a
Jose Hidalgo <c0d5x@protonmail.com>
2016-07-26 11:49:06 -0600

    syntastic

--- python/configure.sh
+++ python/configure.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+CWD=$(pwd -P)
+
+if [ -e ~/.config/flake8 ]; then 
+    rm ~/.config/flake8
+fi
+ln -fs "$CWD/flake8rc" ~/.config/flake8
+
+pip install --upgrade pip
 pip install pdb
-pip install bdb
 pip install neovim
 pip install pep8-naming
 
