199f74c0c61c0540b8fd02219e07698b92cf6b79
Jose Hidalgo <c0d5x@protonmail.com>
2016-08-21 13:59:39 -0600

    runme using python

--- runme.py
+++ runme.py
@@ -1,48 +1,66 @@
 #!/usr/bin/env python
+'''
+Script for managing dotfiles that follow this convention:
+    - Symbolic links (files and directories) have to be called
+        <filename>.symlink
+      And will be linked to:
+        ~/.<filename>
+    - Every file called
+        `uname -o`.sh
+        `uname -o`-update.sh
+      on subdirectories will be executed
+'''
 
 
 import argparse
 import os
 
 
-def link_files():
+def link_files(dotdir, homedir):
     '''
     Links all files or directories with sufix: *.symlink
     '''
-    print('link files')
-    pass
+    for file in os.scandir(dotdir):
+        if file.name.endswith('.symlink'):
+            link_file(file.path, homedir + '/.' + file.name.replace('.symlink', ''))
+        elif file.is_dir():
+            link_files(file.path, homedir)
 
 
-def link_file(src, dst=''):
+def link_file(src, dst):
     '''
     Link one file from the dotdir to the home dir.
-
     Checks if files are actually different, removes dst if necessary.
     '''
-    pass
+    srcinfo = os.stat(path=src)
+    if os.path.exists(dst):
+        dstinfo = os.stat(path=dst)
+        if srcinfo.st_ino == dstinfo.st_ino:
+            return
+        else:
+            os.unlink(dst)
+
+    print("linking %s to %s" % (src, dst))
+    os.symlink(src, dst)
 
 
-def run_scripts():
+def run_scripts(dotdir, sname):
     '''
-    Runs all 'configure.sh' files it finds in subdirectories
+    Runs all '<sname>' files it finds in subdirectories
     '''
-    print('run scripts')
-    pass
+    for file in os.scandir(dotdir):
+        if file.name == sname:
+            run_script(file.path.replace(file.name, ''), file.name)
+        elif file.is_dir():
+            run_scripts(file.path, sname)
 
 
-def run_script(script):
+def run_script(path, script):
     '''
     Runs this script in its subdirectory
     '''
-    pass
-
-
-def validate_dotdir(dir):
-    '''
-    Make sure the dir exists and we can read it all
-    '''
-    print('validating dir: %s' % dir)
-    pass
+    print("Running %s in %s" % (script, path))
+    os.system("cd %s; bash %s; cd - >/dev/null" % (path, script))
 
 
 def parse_args():
@@ -52,17 +70,23 @@ def parse_args():
 
     # default dotfiles dir is the same where this script sits
     dotdir = os.path.dirname(os.path.abspath(__file__))
+    homedir = os.environ['HOME']
+    osname = os.uname().sysname + '.sh'
+    osname_update = os.uname().sysname + '-update.sh'
 
     parser = argparse.ArgumentParser(description='Manage your dotfiles')
     parser.add_argument('-l', '--link', action='store_true',
                         help='link the files in your dotdir to your home dir')
     parser.add_argument('-r', '--run', action='store_true',
                         help='run the scripts in the dotdir subdirectories')
+    parser.add_argument('-u', '--update', action='store_true',
+                        help='run the update scripts in the dotdir subdirectories')
     parser.add_argument('-a', '--all', action='store_true',
                         help='link files, run scripts')
     parser.add_argument('-d', '--dotdir',
                         help='directory for dotfiles, defaults to ' +
                         'directory of this script')
+    parser.add_argument('-p', '--homedir', help='user home directory')
     args = parser.parse_args()
 
     if not (args.all or args.run or args.link):
@@ -72,16 +96,20 @@ def parse_args():
     if args.dotdir:
         dotdir = args.dotdir
 
-    validate_dotdir(dotdir)
+    if args.homedir:
+        homedir = args.homedir
 
     if args.all:
-        link_files()
-        run_scripts()
+        link_files(dotdir, homedir)
+        run_scripts(dotdir, osname)
+        run_scripts(dotdir, osname_update)
     else:
         if args.link:
-            link_files()
+            link_files(dotdir, homedir)
         if args.run:
-            run_scripts()
+            run_scripts(dotdir, osname)
+        if args.update:
+            run_scripts(dotdir, osname_update)
 
 
 if __name__ == "__main__":
