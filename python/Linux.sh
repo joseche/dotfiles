a4fb5b89a6a29b127e40c28b8ba26c92636457df
Jose Hidalgo <c0d5x@protonmail.com>
2016-08-28 00:11:25 -0600

    python unit tests

--- python/Linux.sh
+++ python/Linux.sh
@@ -7,10 +7,23 @@ if [ -e ~/.config/flake8 ]; then
 fi
 ln -fs "$CWD/flake8rc" ~/.config/flake8
 
-sudo pip install --upgrade pip
-sudo pip install pdb
-sudo pip install neovim
-sudo pip install pep8-naming
+sudo easy_install pip
+sudo pip install --upgrade pip --upgrade
+sudo pip install --upgrade pdb
+sudo pip install --upgrade neovim
+sudo pip install --upgrade pep8-naming
+sudo pip install --upgrade flask
+sudo pip install --upgrade nose
+sudo pip install --upgrade nose-cov
+sudo pip install --upgrade rednose
+sudo pip install --upgrade pytest
+sudo pip install --upgrade pytest-cov
+sudo pip install --upgrade mock
+sudo pip install --upgrade requests
+sudo pip install --upgrade Django
 
-sudo apt install python3-pudb
+sudo apt -y autoremove
+sudo apt -y install python3-pudb
+sudo apt -y install virtualenv
+sudo apt -y install virtualenvwrapper
 
