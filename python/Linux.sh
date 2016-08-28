33c02f781cab890d67d2c7d4adf9fd63ac5b7c65
Jose Hidalgo <c0d5x@protonmail.com>
2016-08-27 23:55:05 -0600

    installing linux packages

--- python/Linux.sh
+++ python/Linux.sh
@@ -1,1 +1,16 @@
-Darwin.sh
+#!/bin/sh
+
+CWD=$(pwd -P)
+
+if [ -e ~/.config/flake8 ]; then 
+    rm ~/.config/flake8
+fi
+ln -fs "$CWD/flake8rc" ~/.config/flake8
+
+sudo pip install --upgrade pip
+sudo pip install pdb
+sudo pip install neovim
+sudo pip install pep8-naming
+
+sudo apt install python3-pudb
+
