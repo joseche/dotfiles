d995efbfa587e1a3d379bb2dba24eb73fa16a0cd
Jose Hidalgo <c0d5x@protonmail.com>
2016-08-28 23:21:50 -0600

    using zprezto

--- shell/Linux.sh
+++ shell/Linux.sh
@@ -5,3 +5,11 @@ if [ -z "$ZSH" ]; then
     echo -e "\nShell is NOT zsh, setting shell, it may ask your password"
     chsh -s /usr/local/bin/zsh
 fi
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
