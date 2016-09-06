c9c56d763f38e17c0799d9e07b2b78947defe2df
Jose Hidalgo <c0d5x@protonmail.com>
2016-09-05 22:47:34 -0600

    lubuntu fixes

--- 001-packages/apt.sh
+++ 001-packages/apt.sh
@@ -3,7 +3,7 @@
 sudo apt install -y sysstat lm-sensors sensord vim-gtk3 i2c-tools terminology \
    zsh git python3-pudb virtualenv rubygems ruby-all-dev \
    python-setuptools python-dev build-essential \
-   awscli curl wget nmap
+   awscli curl wget nmap exuberant-ctags
 
 sudo apt -y autoremove
 
