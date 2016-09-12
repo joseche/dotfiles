b00e88cc6bcb2ccbde4014e830f2a231f7bdb4d2
Jose Hidalgo <c0d5x@protonmail.com>
2016-09-12 09:40:14 -0600

    adding atom - why not

---
+++ atom/Linux.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+which apm && ruby -e '%w{
+autocomplete-go
+builder-go
+environment
+figletify
+git-plus
+go-plus
+language-chef
+language-hcl
+language-puppet
+language-terraform
+language-viml
+linter
+linter-foodcritic
+linter-puppet
+minimap
+navigator-godef
+pretty-json
+}.each do |c|
+  cmd = "apm install #{c}"
+  %x( #{cmd} )
+end'
+
