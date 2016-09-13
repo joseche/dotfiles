a7b0433909dd48d0508bc513e6b80c4e89ff5cb5
Jose Hidalgo <c0d5x@protonmail.com>
2016-09-12 18:06:26 -0600

    virtualbox and others

--- 001-packages/apt.sh
+++ 001-packages/apt.sh
@@ -4,7 +4,11 @@ sudo apt install -y sysstat lm-sensors sensord vim-gtk3 i2c-tools terminology \
    zsh git python3-pudb virtualenv rubygems ruby-all-dev \
    python-setuptools python-dev build-essential \
    awscli curl wget nmap exuberant-ctags wifite wireshark \
-   macchanger bpython3
+   macchanger bpython3 gitk virtualbox-qt virtualbox-ext-pack \
+   virtualbox-dkms virtualbox-guest-dkms virtualbox-guest-x11 \
+   virtualbox-guest-utils libvirt-bin vagrant vagrant-lxc \
+   qemu-kvm mailutils lxctl virtualbox-guest-additions-iso \
+   sqlite3 virtualbox 
 
 sudo apt -y autoremove
 
