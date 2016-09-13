379d30a8f2ba2ae66aadcf550aa2c7f4a05c6c9d
Jose Hidalgo <c0d5x@protonmail.com>
2016-09-12 19:53:35 -0600

    lubuntu fixes

--- shell/Darwin.sh
+++ shell/Darwin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ZSH=$(echo $SHELL|grep zsh)
-if [ -z "$ZSH" ]; then 
+if [ -z "$ZSH" ]; then
     echo -e "\nShell is NOT zsh, setting shell, it may ask your password"
     chsh -s /usr/local/bin/zsh
 fi
