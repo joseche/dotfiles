13b93fc3a55c82d1f302ef7663f323662dc2e845
Jose Hidalgo <joseche@gmail.com>
2024-08-19 22:37:05 +0200

    added bufferline for tabs

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -26,16 +26,16 @@ vim.opt.syntax = 'on'
 vim.cmd("filetype plugin indent on")
 
 -- move easily pressing option/alt
-vim.api.nvim_set_keymap("n", "<M-Left>", ":wincmd h<CR>",  { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<M-Right>", ":wincmd l<CR>",  { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<M-Up>", ":wincmd k<CR>",  { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<M-Down>", ":wincmd d<CR>",  { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Left>", ":wincmd h<CR>",  { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Right>", ":wincmd l<CR>",  { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Up>", ":wincmd k<CR>",  { noremap = true, silent = true })
+vim.keymap.set("n", "<M-Down>", ":wincmd d<CR>",  { noremap = true, silent = true })
 
 -- vim.cmd.colorscheme('torte')
 
 -- Leader key
 vim.g.mapleader = " " -- Space as the leader key
-vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>q", ":q<CR>", { noremap = true, silent = true })
 
 require("lazy-configuration")
