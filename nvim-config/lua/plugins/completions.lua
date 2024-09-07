b784ec7996d4f46bf4ac0b7498cb8eb442862f07
Jose Hidalgo <joseche@gmail.com>
2024-09-07 23:26:16 +0200

    disable spell

--- nvim-config/lua/plugins/completions.lua
+++ nvim-config/lua/plugins/completions.lua
@@ -86,7 +86,7 @@ return {
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.completion.spell,
+                    -- null_ls.builtins.completion.spell,
                     null_ls.builtins.formatting.stylua,
                     null_ls.builtins.formatting.isort,
                     null_ls.builtins.formatting.black,
